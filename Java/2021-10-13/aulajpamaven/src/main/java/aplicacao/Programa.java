90c4f5ec9146dea85b4712d3f2e78909cce8c086
Fernando Candido de Siqueira Junior <90061495+fernandocandidodesiqueirajunior@users.noreply.github.com>
2021-10-13 17:46:59 -0400

    JPA
    Projeto de jpa - introdução

---
+++ Java/2021-10-13/aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,47 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import Dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		
+		inserir();
+		
+
+
+	}
+	
+		private static void inserir() {
+			
+			Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+			Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+			Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+			
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+			EntityManager em = emf.createEntityManager();
+			
+			em.getTransaction().begin();
+			em.persist(p1);
+			em.persist(p2);
+			em.persist(p3);
+			em.getTransaction().commit();
+			
+			em.close();
+			emf.close();
+			
+			
+			System.out.println(p1);
+			System.out.println(p2);
+			System.out.println(p3);
+			System.out.println("Finalizado");
+				
+			
+		}
+
+}
